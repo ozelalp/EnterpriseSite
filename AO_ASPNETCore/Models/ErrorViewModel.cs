2ca16a65e8a4600f6882309891d12b6d1826ee22
Alper Ozel <alper.ozel@thomsonreuters.com>
2019-10-30 10:52:00 +0300

    added new projects

---
+++ AO_ASPNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AO_ASPNETCore.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
